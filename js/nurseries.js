71448b0d8f0a77796173c3420d2811a3f5c1f08a
indiajohnson-cm <46683218+indiajohnson-cm@users.noreply.github.com>
2020-03-28 16:35:03 -0400

    Add test nursery

--- js/nurseries.js
+++ js/nurseries.js
@@ -78,6 +78,38 @@ var geoJson = ({
                 ]
             }
         },
+                {
+            'type': 'Feature',
+            'properties': {
+                'Timestamp': '10/21/2019 12:28:29',
+                'Nursery_Status': 'New Nursery',
+                'Nursery': 'Plants and Posies',
+                'State_1': 'OH',
+                'Type': 'Wholesale, Retail, Online, Open to the Public',
+                'Native _%': '200%',
+                'Native': 'Yes',
+                'Keep': 'Seed, Plants, Herbaceous, Ferns, Woody, Shrubs, Bare Root, Containerized Trees, B&B Trees',
+                'Grow_Type': 'From known seed locations, Grown from seed',
+                'Specialty': 'Organic, Wetland Restoration, Native Plants, Bioengineering Material',
+                'Address': '4202 Fulton rd. ',
+                'Owner_Con': 'India Johnson',
+                'Contact_Number': 2169037771,
+                'Contact_Email': 'itj3@case.edu',
+                'Preferred_Contact': 'Phone, Email',
+                'Notes': 'This is a test',
+                'Questions and comments': 'Do you even host brah?',
+                'Zip': 44144,
+                'point': (40.90153052205019, -81.88792299659391, 0.0),
+                'Latitude': 40.90153052205019,
+                'Longitude': -81.88792299659391,
+                'elevation': 0.0
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [-81.88792299659391, 40.90153052205019]
+            }
+        },
+                 
         {
             "type": "Feature",
             "properties": {
